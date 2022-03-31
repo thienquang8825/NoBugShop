6e9a3ca31e9bb5fae91d2f3765ec3e8a952f9068
thienquang8825 <thienquang8825@gmail.com>
2022-03-31 17:34:37 +0700

    Express

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import { moneyFormat } from '../utils/moneyFormat'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = () => {
   const { id } = useParams()
 
-  const product = products.find((p) => p._id === id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+    fetchData()
+  }, [id, product])
 
   return (
     <>
--- frontend/src/utils/moneyFormat.js
+++ frontend/src/utils/moneyFormat.js
@@ -1,4 +1,4 @@
-export const moneyFormat = (money) => {
+export const moneyFormat = (money = 0) => {
   return money.toLocaleString('vi-VN', {
     style: 'currency',
     currency: 'VND',
